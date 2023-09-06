0974fc2bc054dd8acb7c7e9fb58b6551764c050c
MariaClaraBermudez <mariaclarabermudez10@gmail.com>
2023-09-06 15:59:54 -0300

    atualizei challenge

--- projeto.js
+++ projeto.js
@@ -9,3 +9,15 @@ selectCorFundo.addEventListener("change", function () {
 
 //adiciona um ouvinte de evento CHANGE ao seletor personalizado.
 //Quando o evento CHANGE é acionado, a cor selecionada é obtida usando "selectCorFundo.value", e a cor de fundo do próprio seletor personalizado é atualizada com a cor selecionada usando "selectCorFundo.style.backgroundColor". Isso cria o efeito de mudança de cor na própria caixa de seleção quando uma cor é selecionada.
+
+
+var editor = CodeMirror.fromTextArea(document.getElementById("codigo"), {
+    mode: "htmlmixed", // Reconhece HTML, CSS e JavaScript
+    lineNumbers: true,
+    theme: "blackboard" // Usando o tema blackboard
+    
+});
+
+// Definindo o fundo preto para a caixa de código
+editor.getWrapperElement().style.backgroundColor = "black";
+
